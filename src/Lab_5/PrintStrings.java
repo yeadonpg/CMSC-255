fe266f01f6aeaf9ea235bb8aba85fe88d6ebe3a7
Patrick Yeadon <patrick-yeadon@users.noreply.github.com>
2019-09-24 14:06:07 -0400

    Completed Lab_5

---
+++ src/Lab_5/PrintStrings.java
@@ -0,0 +1,18 @@
+package Lab_5;
+
+import java.util.Scanner;
+
+public class PrintStrings {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter two words: ");
+        String[] words = input.nextLine().trim().split("\\s");
+        String word1 = words[0];
+        String word2 = words[1];
+        if (word1.compareTo(word2) < 0) {
+            System.out.println(word1 + "\n" + word2);
+        } else {
+            System.out.println(word2 + "\n" + word1);
+        }
+    }
+}
