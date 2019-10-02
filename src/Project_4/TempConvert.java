6961fc9327221520dc7ca4a8674120bf4d6db2e2
yeadonpg@mymail.vcu.edu <yeadonpg@mymail.vcu.edu>
2019-10-02 19:55:37 -0400

    Completed algorithm; Added comments; Project 4 Finished!

--- src/Project_4/TempConvert.java
+++ src/Project_4/TempConvert.java
@@ -16,63 +16,119 @@ public class TempConvert {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String userAnswer = "";
+        String userAnswer;
         do {
+
+            /*
+            * processData () { ... }
+             */
+
             processData(in);
+
+            /*
+            *   PRINT "Do you want to convert another temperature amount? Enter quit to exit"
+            *   userAnswer = INPUT
+             */
+
             System.out.println("Do you want to convert another temperature amount? Enter quit to exit.");
             userAnswer = in.nextLine();
+
         } while (!userAnswer.equals("quit"));
     }
 
     public static void processData(Scanner in) {
+
+        /*
+        * PRINT "Enter the temperature to convert:"
+        * double userTemp = INPUT
+         */
+
         System.out.println("Enter the temperature to convert:");
-        double userTemp = in.nextInt();
+        double userTemp = in.nextDouble();
         in.nextLine();
+
+        /*
+        * PRINT "Enter the current scale of the temperature:"
+        * String userScale = INPUT
+         */
+
         System.out.println("Enter the current scale of the temperature:");
         String userScale = in.nextLine();
+
+        /*
+        * PRINT "Enter the scale you want to convert the temperature to:"
+        * String userConvert = INPUT
+         */
+
         System.out.println("Enter the scale you want to convert the temperature to:");
         String userConvert = in.nextLine();
+
+        /*
+        * boolean valid = checkValidity (userTemp, userScale) { ... }
+        * IF (valid) { ... }
+         */
+
         if (checkValidity(userTemp, userScale)) {
             double convertedTemp = 0;
             if (!checkValidity(0, userConvert)) {
                 System.out.printf("%s is not a valid conversion scale\n", userConvert);
                 return;
             }
+
+            /*
+            * // Perform the actual calculations via one of the 6 methods
+            * convertedTemp = convert()
+            * PRINT "{userTemp} degrees {userScale} is {convertedTemp} degrees {userConvert}"
+             */
+
+            // These nested switches determine the right method to use for converting
             switch (userScale) {
+                // User is converting from Fahrenheit
                 case "Fahrenheit":
                     switch (userConvert) {
+                        // Converting to Celsius
                         case "Celsius":
                             convertedTemp = convertFahtoCel(userTemp);
                             break;
+                        // Converting to Kelvin
                         case "Kelvin":
                             convertedTemp = convertFahtoKel(userTemp);
                             break;
+                        // Converting to the same scale
                         default:
                             convertedTemp = userTemp;
                             break;
                     }
                     break;
+                // User is converting from Celsius
                 case "Celsius":
                     switch (userConvert) {
+                        // Converting to Fahrenheit
                         case "Fahrenheit":
                             convertedTemp = convertCeltoFah(userTemp);
                             break;
+                        // Converting to Kelvin
                         case "Kelvin":
                             convertedTemp = convertCeltoKel(userTemp);
                             break;
+                        // Converting to the same scale
                         default:
                             convertedTemp = userTemp;
                             break;
                     }
                     break;
+                // User is converting from Kelvin
                 case "Kelvin":
                     switch (userConvert) {
+                        // Converting to Fahrenheit
                         case "Fahrenheit":
                             convertedTemp = convertKeltoFah(userTemp);
                             break;
+                        // Converting to Celsius
                         case "Celsius":
                             convertedTemp = convertKeltoCel(userTemp);
                             break;
+                        // Converting to the same scale
                         default:
                             convertedTemp = userTemp;
                             break;
@@ -81,10 +137,32 @@ public class TempConvert {
             }
             System.out.printf("%.2f degrees %s is %.2f degrees %s\n", userTemp, userScale, convertedTemp, userConvert);
         }
+
+        /*
+        * ELSE {
+        *   PRINT "{userTemp} degrees {userScale} is not a valid temperature"
+        * }
+         */
+
         else {
             System.out.println(userTemp + " degrees " + userScale + " is not a valid temperature");
         }
+
     }
+
+    /*
+    * checkValidity (userTemp, userScale) {
+    *    IF ((userScale == "Fahrenheit") && (-479.4 <= userTemp <= 1000)) {
+    *        return true;
+    *    } ELSE IF ((userScale == "Celsius") && (-273 <= userTemp <= 1000)) {
+    *        return true;
+    *    } ELSE IF ((userScale == "Kelvin") && (0 <= userTemp <= 1000)){
+    *        return true;
+    *    } ELSE {
+    *        return false;
+    *    } END IF
+    * } END checkValidity
+     */
 
     public static boolean checkValidity(double temp, String scale) {
         switch (scale) {
